ff647c08db434dc5b84e714b9d282db408254cbf
Steve Derico <steve@stevederico.com>
2012-06-01 01:05:46 -0700

    Added New SDKit.h Import Statement

--- SDSampleApp/SDFooterButtonViewController.m
+++ SDSampleApp/SDFooterButtonViewController.m
@@ -5,7 +5,7 @@
 //  Created by Stephen Derico on 5/27/12.
 //  Copyright (c) 2012 Bixby Apps. All rights reserved.
 //
-#import "SDFooterButtonView.h"
+#import "SDKit.h"
 #import "SDFooterButtonViewController.h"
 
 @interface SDFooterButtonViewController ()
